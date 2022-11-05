f3b94071798c034dbffb5f564f7feb13109d951b
Joseph Izarra <New.Jeizher@gmail.com>
2022-11-05 17:10:02 -0430

    agregando el menu en mobile

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const navEmail=document.querySelector('.navbar-email');
-const desktopMenu=document.querySelector('.desktop-menu');
+const navEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
 
-navEmail.addEventListener('click',toggleDesktopmenu)
+const navMobile = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+navEmail.addEventListener('click', toggleDesktopmenu);
+navMobile.addEventListener('click', toggleMobiletopmenu);
 
 
-function toggleDesktopmenu(){
-desktopMenu.classList.toggle('inactive')
+function toggleDesktopmenu() {
+    desktopMenu.classList.toggle('inactive');
+}
 
-
+function toggleMobiletopmenu() {
+    mobileMenu.classList.toggle('inactive');
 }
