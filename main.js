0fe2a46624f1477feabb550c62aa011f2b31049f
Joseph Izarra <New.Jeizher@gmail.com>
2022-11-08 11:37:02 -0430

    cambio de datos al seleccionar un producto

--- main.js
+++ main.js
@@ -10,6 +10,13 @@ const productDetailCloseIcon=document.querySelector('.product-detail-close');
 const navShoppingCart = document.querySelector('.navbar-shopping-cart');
 const shopingCartContainer = document.querySelector('#shopingCartContainer');
 const productDetailContainer= document.querySelector('#productDetail');
+
+const producDetailImg=document.querySelector('#image');
+const producDetailPrice=document.querySelector('#precio');
+const producDetailName=document.querySelector('#nombre');
+const producDetailDescription=document.querySelector('#descripcion');
+
+
 const cardsContainer=document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopmenu);
@@ -27,7 +34,7 @@ function toggleDesktopmenu() {
 function toggleMobiletopmenu() {
     mobileMenu.classList.toggle('inactive');
     shopingCartContainer.classList.add('inactive');
-
+    productDetailContainer.classList.add('inactive');
 }
 
 function toggleShoppingCart() {
@@ -39,10 +46,16 @@ function toggleShoppingCart() {
 
 }
 
-function clickEnProducto(){
+function clickEnProducto(image , precio , nombre , descripcion){
     productDetailContainer.classList.remove('inactive');
     shopingCartContainer.classList.add('inactive');
     desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+
+    producDetailImg.setAttribute('src', image);
+    producDetailPrice.innerText = '$' + precio;
+    producDetailName.innerText = nombre;
+    producDetailDescription.innerText = descripcion;
 
 }
 
@@ -55,37 +68,44 @@ const productList = []
 productList.push({
     name: 'bike',
     price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg'
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg',
+    descripcion: 'excelente bicicleta bla bla bla'
 });
 productList.push({
     name: 'Bicycle helmet',
     price: 1600,
-    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg',
+    descripcion: 'Maxima proteccion bla bla y bla bla'
 });
 productList.push({
     name: 'Bicycle helmet',
     price: 1500,
-    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
+    descripcion: 'otro casco bonito bla bla'
 });
 productList.push({
     name: 'Seat',
     price: 300,
-    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg',
+    descripcion:'el mejor asiento de bici bla bla bla'
 });
 productList.push({
     name: 'Tennis Montain Bike',
     price: 2200,
-    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg',
+    descripcion:'El mejor tennis para montar bici bla bla bla'
 });
 productList.push({
     name: 'Sunglasses',
     price: 800,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602',
+    descripcion:'proteccion para la vista los mejores lentes de sol bla bla bla'
 });
 productList.push({
     name: 'Sunglasses',
     price: 600,
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603',
+    descripcion:'otros maravillosos lentes de so bla bla bla'
 });
 
 // crear el html del producto desde JS 
@@ -104,7 +124,7 @@ productList.push({
 </div> */
 
 function renderproduct(array){
-for (produc of array) {
+for (let produc of array) {
 
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
@@ -113,7 +133,9 @@ for (produc of array) {
 
     const productImage = document.createElement('img');
     productImage.setAttribute('src', produc.image);
-    productImage.addEventListener('click', clickEnProducto);
+   
+    productImage.addEventListener('click',function(){clickEnProducto(produc.image,produc.price,produc.name,produc.descripcion)} );
+   
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -148,6 +170,14 @@ for (produc of array) {
 }
 
 renderproduct(productList);
+
+// function ActualizaInfo(image){
+
+//     producDetailImg.setAttribute('src', image);
+//     // producDetailPrice.innerText = '$' + precio
+//     // producDetailRightAsideName.innerText = nombre
+//     // producDetailRightAsideDescription.innerText = descripcion
+// }
 
 
 
